2532abe19ec7105cab69dbc25d3197a5ac468a8c
TheJuliuscanute <juliuscanute@touchcapture.net>
2019-07-06 19:02:53 +1000

    [AB#22] Loads images lazily

--- app/src/main/java/com/julius/anoma/ui/FeedAdapter.kt
+++ app/src/main/java/com/julius/anoma/ui/FeedAdapter.kt
@@ -1,5 +1,7 @@
 package com.julius.anoma.ui
 
+import android.content.Context
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,8 +10,13 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.julius.anoma.R
 import com.julius.anoma.repository.Feed
+import com.squareup.picasso.Picasso
+import jp.wasabeef.picasso.transformations.RoundedCornersTransformation
+import java.lang.Exception
 
-class FeedAdapter(private val feedItems: List<Feed>) : RecyclerView.Adapter<FeedAdapter.ViewHolder>() {
+class FeedAdapter(private val feedItems: List<Feed>, private val context: Context) :
+    RecyclerView.Adapter<FeedAdapter.ViewHolder>() {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         val listItem = layoutInflater.inflate(R.layout.feed_item_card, parent, false)
@@ -29,10 +36,32 @@ class FeedAdapter(private val feedItems: List<Feed>) : RecyclerView.Adapter<Feed
         private val heading: TextView = itemView.findViewById(R.id.heading)
         private val content: TextView = itemView.findViewById(R.id.content)
         private val thumbnail: ImageView = itemView.findViewById(R.id.thumbnail)
+
         fun bind(feedItem: Feed) {
             heading.text = feedItem.title
             content.text = feedItem.description
-            thumbnail.setImageResource(R.drawable.ic_broken_image_placeholder)
+            loadThumbnail(feedItem)
+        }
+
+        private fun loadThumbnail(feedItem: Feed) {
+            if (feedItem.imageHref.isNotEmpty()) {
+                try {
+                    val uri = Uri.parse(feedItem.imageHref)
+                    Picasso.get()
+                        .load(uri)
+                        .transform(
+                            RoundedCornersTransformation(
+                                context.resources.getDimensionPixelSize(R.dimen.image_corner_radius),
+                                context.resources.getDimensionPixelSize(R.dimen.image_corner_margin)
+                            )
+                        )
+                        .error(R.drawable.ic_broken_image_placeholder)
+                        .placeholder(R.drawable.ic_crop_original)
+                        .into(thumbnail)
+                } catch (e: Exception) {
+                    thumbnail.setImageResource(R.drawable.ic_broken_image_placeholder)
+                }
+            }
         }
     }
 }
